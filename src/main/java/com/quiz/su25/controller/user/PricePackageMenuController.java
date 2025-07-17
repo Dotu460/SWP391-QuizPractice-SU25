1a1b70443e2754f4a9c42c94fd1856016f9885d9
quangmingdoc <nguyenqminh16@gmail.com>
2025-07-17 17:01:38 +0700

    feat: added price package menu for user to choose and pick whatever package they want

---
+++ src/main/java/com/quiz/su25/controller/user/PricePackageMenuController.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.quiz.su25.controller.user;
+
+import com.quiz.su25.dal.impl.PricePackageDAO;
+import com.quiz.su25.entity.PricePackage;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ *
+ * @author kenngoc
+ */
+@WebServlet(name = "PricePackageMenuController", urlPatterns = {"/price-package-menu"})
+public class PricePackageMenuController extends HttpServlet {
+    private PricePackageDAO pricePackageDAO;
+
+    @Override
+    public void init() throws ServletException {
+        pricePackageDAO = new PricePackageDAO();
+    }
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Lấy danh sách các gói active
+        List<PricePackage> pricePackages = pricePackageDAO.findPricePackagesWithFilters(
+            "active", null, null, null, 1, Integer.MAX_VALUE
+        );
+        request.setAttribute("pricePackages", pricePackages);
+        request.getRequestDispatcher("/view/user/price_package/price-package-menu.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
